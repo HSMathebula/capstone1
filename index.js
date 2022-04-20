4a68d68a1c3d488b15a8504a5a4a0cfe2090ca14
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-04-20 22:41:15 +0200

    Add read more function with js

---
+++ index.js
@@ -0,0 +1,31 @@
+const mobileBtn = document.getElementById('mobileMenu');
+
+const nav = document.querySelector('.nav-list');
+
+const mobileBtnExit = document.getElementById('mobile-exit');
+
+mobileBtn.addEventListener('click', () => {
+  nav.classList.add('menu-btn');
+});
+
+mobileBtnExit.addEventListener('click', () => {
+  nav.classList.remove('menu-btn');
+});
+
+nav.addEventListener('click', () => {
+  nav.classList.remove('menu-btn');
+});
+
+// Read more btn
+function ReadMore() {
+    let moreText = document.getElementById("more");
+    let toggle = document.getElementById('toggle');
+  
+    if (moreText.style.display !== "none") {
+        toggle.innerHTML = "MORE"; 
+        moreText.style.display = "none";
+    } else {
+        toggle.innerHTML = "LESS"; 
+        moreText.style.display = "block";
+    }
+  }
